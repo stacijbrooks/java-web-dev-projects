038645969a7d52a32630fd65b437bc0db6a5d42f
stacijbrooks <stacibrooksfitness@gmail.com>
2024-03-31 20:31:24 -0500

    Rectangle.java

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/Rectangle.java
@@ -0,0 +1,21 @@
+package org.launchcode;
+
+
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the length of the rectangle:");
+        double length = input.nextDouble();
+
+        System.out.println("Please enter the width of the rectangle:");
+        double width = input.nextDouble();
+
+        double area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
