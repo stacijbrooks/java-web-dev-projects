0e0487ea357b0ab3ca8a882b5ebf49b3c5d914f1
stacijbrooks <stacibrooksfitness@gmail.com>
2024-04-16 19:49:06 -0500

    Inheritance Main

---
+++ inheritance/testing-inheritance/exercises/src/main/java/Main.java
@@ -0,0 +1,10 @@
+//import Cat.java;
+//
+//import HouseCat.java;
+
+
+public class Main {
+    HouseCat garfield = new HouseCat("Garfield", 12);
+garfield.eat();
+System.out.println(garfield.isTired());   // prints true
+}
